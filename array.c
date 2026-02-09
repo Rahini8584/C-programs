768ca1d0aea42932dcfa6cbd731c36c0b5b8f06a
Rahini <rahinikalra6@gmail.com>
2026-02-09 10:07:15 +0530

    loop question 6

--- array.c
+++ array.c
@@ -120,15 +120,24 @@ int main(){
     // }
 
 
-    int n ; 
-    scanf("%d", &n);
-    int i , j;
-    for(i=1;i<=n;i++){
-        for(j=1;j<=n;j++){
-            printf("* ");
-        }
-        printf("\n");
-    }
+    // int n ; 
+    // scanf("%d", &n);
+    // int i , j;
+    // for(i=1;i<=n;i++){
+    //     for(j=1;j<=n;j++){
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
+    // int n ;
+    // scanf("%d", &n);
+    // int i, j;
+    // for(i=1;i<=n;i++){
+    //     for(j=1;j<=i;j++){
+    //         printf("%d ",j);
+    //     }
+    //     printf("\n");
+    // }
     return 0;
 
 
