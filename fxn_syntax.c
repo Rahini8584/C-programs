b007d69325623032728def1124bea5081919a6af
Rahini <rahinikalra6@gmail.com>
2026-03-09 15:07:59 +0530

    function-ques-1

--- fxn_syntax.c
+++ fxn_syntax.c
@@ -14,3 +14,26 @@
 // 2. NR WA
 // 3. WR NA
 // 4. WR WA
+
+
+#include<stdio.h>
+// void add();
+// void add2( int, int);
+int main(){
+    // add();
+    // printf("\n");
+    // int a, b;
+    // scanf("%d %d",&a,&b);
+    // add2(a,b);
+    return 0 ;
+}
+
+// void add(){
+//     int a, b;
+//     scanf("%d %d",&a,&b);
+//     printf("Sum is %d",a+b);
+// }
+
+// void add2(int x,int y){
+//     printf("sum= %d",x+y);
+// }
