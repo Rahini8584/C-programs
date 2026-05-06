130848f879099b0157e1541955395f4b066f7bbc
Rahini <rahinikalra6@gmail.com>
2026-05-06 09:47:13 +0530

    -

--- pointers.c
+++ pointers.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-// POINTER is a variable used to store address of a variable
+// #include<stdio.h>
+// // POINTER is a variable used to store address of a variable
 
-int main(){
-    int a= 10;
-    int *p = &a;
-    printf("%d\n",a);
-    printf("%p\n",p);
+// int main(){
+//     int a= 10;
+//     int *p = &a;
+//     printf("%d\n",a);
+//     printf("%p\n",p);
 
-    return 0;
-}
+//     return 0;
+// }
